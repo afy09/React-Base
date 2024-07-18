25e1c7ad7dc2c715bd479fa8c43b39c2455ea3f6
budi@gmail.com <mhmd.raffli09@gmail.com>
2024-07-18 11:15:34 +0700

    modifed file intro.js

--- 6-afilm-react-2/src/components/Intro.jsx
+++ 6-afilm-react-2/src/components/Intro.jsx
@@ -9,7 +9,7 @@ const Intro = () => {
             <div className="title">NONTON GRATIS</div>
             <div className="title">GAK PAKE KARCIS</div>
             <div className="introButton mt-4 text-center">
-              <Button variant="dark">Lihat Semua List</Button>
+              <Button variant="dark">Semua List</Button>
             </div>
           </Col>
         </Row>
